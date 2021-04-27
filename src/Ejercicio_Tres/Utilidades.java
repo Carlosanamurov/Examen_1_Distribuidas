df66cc506b4685c7e352e8ae659d5ce3e0543ccf
Carlosanamurov <carlosanamuroventura@upeu.edu.pe>
2021-04-27 16:44:20 -0500

    ejercicios

---
+++ src/Ejercicio_Tres/Utilidades.java
@@ -0,0 +1,15 @@
+package Ejercicio_Tres;
+
+public class Utilidades {
+
+	public static int numAzar(int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void esperarTiempoAzar(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
